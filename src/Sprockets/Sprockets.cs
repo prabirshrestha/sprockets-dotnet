950bb6e484c1dfa6ca61be1ee36943fd60d18e64
prabirshrestha <mail@prabir.me>
2012-09-21 22:00:49 -0400

    Sprockets.cs

---
+++ src/Sprockets/Sprockets.cs
@@ -0,0 +1,13 @@
+﻿namespace Sprockets
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Sprockets
+    {
+        public virtual IList<IDictionary<string, object>> Get(string file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
