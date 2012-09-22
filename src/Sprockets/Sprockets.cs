ab1b7cb91e2b5c48943389ecaee2d3a671faec4c
prabirshrestha <mail@prabir.me>
2012-09-21 22:26:23 -0400

    assets

--- src/Sprockets/Sprockets.cs
+++ src/Sprockets/Sprockets.cs
@@ -2,12 +2,24 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
+    using System.Text.RegularExpressions;
 
     public class Sprockets
     {
+        private static readonly Regex SprocketsRegex =
+            new Regex(@"^//= (?<type>(require_tree|require_self|require)).(?<path>.*)",
+                      RegexOptions.Compiled | RegexOptions.Multiline);
+
         public virtual IList<IDictionary<string, object>> Get(string file)
         {
+            var contents = GetContents(file);
             throw new NotImplementedException();
+        }
+
+        protected string GetContents(string file)
+        {
+            return File.ReadAllText(file);
         }
     }
 }
