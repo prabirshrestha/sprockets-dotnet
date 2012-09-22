f19c76adc3049a3a64a10bcc14bf02067ecb0c11
prabirshrestha <mail@prabir.me>
2012-09-21 22:03:36 -0400

    added CircularDependencyException and Node

---
+++ src/Sprockets/CircularDependencyException.cs
@@ -0,0 +1,19 @@
+﻿namespace Sprockets
+{
+    using System;
+
+    public class CircularDependencyException<T> : Exception
+    {
+        private readonly Node<T> a;
+        private readonly Node<T> b;
+
+        public CircularDependencyException(Node<T> a, Node<T> b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public Node<T> A { get { return this.a; } }
+        public Node<T> B { get { return this.b; } }
+    }
+}
---
+++ src/Sprockets/Node.cs
@@ -0,0 +1,58 @@
+﻿namespace Sprockets
+{
+    using System.Collections.Generic;
+
+    public class Node<T>
+    {
+        private readonly string name;
+        private readonly IList<Node<T>> edges;
+
+        public Node(string name)
+        {
+            this.name = name;
+            this.edges = new List<Node<T>>();
+        }
+
+        public string Name { get { return this.name; } }
+        public IList<Node<T>> Edges { get { return this.edges; } }
+
+        public void DependsOn(params Node<T>[] on)
+        {
+            if (on != null)
+            {
+                foreach (var node in on)
+                {
+                    this.edges.Add(node);
+                }
+            }
+        }
+
+        public T Data { get; set; }
+
+        public static void ResolveDependencies(Node<T> node, List<Node<T>> resolved)
+        {
+            ResolveDependenciesInternal(node, resolved, new List<Node<T>>());
+        }
+
+        private static void ResolveDependenciesInternal(Node<T> node, List<Node<T>> resolved, List<Node<T>> unresolved)
+        {
+            // A software package can be installed when all of its dependencies have been installed, or when it doesn't have any dependencies at all.
+            // When a package has already been resolved, we don't need to visit it again.
+            // A circular dependency is occurring when we see a software package more than once, unless that software package has all its dependencies resolved.
+
+            unresolved.Add(node);
+            foreach (var edge in node.Edges)
+            {
+                if (!resolved.Contains(edge))
+                {
+                    if (unresolved.Contains(edge))
+                        throw new CircularDependencyException<T>(node, edge);
+                    ResolveDependenciesInternal(edge, resolved, unresolved);
+                }
+            }
+
+            resolved.Add(node);
+            unresolved.Remove(node);
+        }
+    }
+}
